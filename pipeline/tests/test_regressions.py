3b33f917f1698a3aed224753f1382fd16342e0f9
Andrew Davison <andrew.davison@cnrs.fr>
2023-09-30 19:43:31 +0200

    Changes also fix #7

--- pipeline/tests/test_regressions.py
+++ pipeline/tests/test_regressions.py
@@ -1,4 +1,6 @@
-from openminds import IRI
+import json
+import os
+from openminds import Collection, IRI
 from openminds.latest import core as omcore
 from utils import build_fake_node
 
@@ -55,3 +57,92 @@ def test_issue_0003():
         == node2.to_jsonld(include_empty_properties=False)
         == expected
     )
+
+
+def test_issue0007():
+    # https://github.com/openMetadataInitiative/openMINDS_Python/issues/7
+    # Instances of embedded types with value type "array" are not correctly resolved for saving and causing an error.
+
+    person = omcore.Person(given_name="A", family_name="Professor", id="_:001")
+    uni1 = omcore.Organization(full_name="University of This Place", id="_:002")
+    uni2 = omcore.Organization(full_name="University of That Place", id="_:003")
+    person.affiliation = [
+        omcore.Affiliation(member_of=uni1),
+        omcore.Affiliation(member_of=uni2),
+    ]
+
+    actual = person.to_jsonld(
+        include_empty_properties=False, embed_linked_nodes=False, with_context=True
+    )
+    expected = {
+        "@context": {"vocab": "https://openminds.ebrains.eu/vocab/"},
+        "@id": "_:001",
+        "@type": "https://openminds.ebrains.eu/core/Person",
+        "vocab:familyName": "Professor",
+        "vocab:givenName": "A",
+        "vocab:affiliation": [
+            {
+                "@type": "https://openminds.ebrains.eu/core/Affiliation",
+                "vocab:memberOf": {
+                    "@id": "_:002",
+                    "@type": "https://openminds.ebrains.eu/core/Organization",
+                },
+            },
+            {
+                "@type": "https://openminds.ebrains.eu/core/Affiliation",
+                "vocab:memberOf": {
+                    "@id": "_:003",
+                    "@type": "https://openminds.ebrains.eu/core/Organization",
+                },
+            },
+        ],
+    }
+    assert actual == expected
+
+    c = Collection(person, uni1, uni2)
+    output_paths = c.save(
+        "issue0007.jsonld", individual_files=False, include_empty_properties=False
+    )
+    assert output_paths == ["issue0007.jsonld"]
+
+    with open(output_paths[0]) as fp:
+        saved_data = json.load(fp)
+    os.remove("issue0007.jsonld")
+    expected_saved_data = {
+        "@context": {"vocab": "https://openminds.ebrains.eu/vocab/"},
+        "@graph": [
+            {
+                "@id": "_:001",
+                "@type": "https://openminds.ebrains.eu/core/Person",
+                "vocab:affiliation": [
+                    {
+                        "@type": "https://openminds.ebrains.eu/core/Affiliation",
+                        "vocab:memberOf": {
+                            "@id": "_:002",
+                            "@type": "https://openminds.ebrains.eu/core/Organization",
+                        },
+                    },
+                    {
+                        "@type": "https://openminds.ebrains.eu/core/Affiliation",
+                        "vocab:memberOf": {
+                            "@id": "_:003",
+                            "@type": "https://openminds.ebrains.eu/core/Organization",
+                        },
+                    },
+                ],
+                "vocab:familyName": "Professor",
+                "vocab:givenName": "A",
+            },
+            {
+                "@id": "_:002",
+                "@type": "https://openminds.ebrains.eu/core/Organization",
+                "vocab:fullName": "University of This Place",
+            },
+            {
+                "@id": "_:003",
+                "@type": "https://openminds.ebrains.eu/core/Organization",
+                "vocab:fullName": "University of That Place",
+            },
+        ],
+    }
+    assert saved_data == expected_saved_data
