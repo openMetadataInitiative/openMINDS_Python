f2021c74a8c164f03f4dd971b6146ab4f800058a
openMINDS pipeline <openminds@ebrains.eu>
2024-03-29 15:32:56 +0000

    build triggered by submodule  version

--- openminds/__init__.py
+++ openminds/__init__.py
@@ -4,7 +4,7 @@ openMINDS Python package
 # Copyright (c) 2023 openMetadataInitiative
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from .base import Node, EmbeddedMetadata, LinkedMetadata, IRI
 from .collection import Collection
