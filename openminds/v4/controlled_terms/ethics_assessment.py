924a70115f68af264faf7dbfc95a377a5b0505de
openMINDS pipeline <openminds@ebrains.eu>
2025-11-25 16:37:34 +0000

    build triggered by workflow_dispatch

--- openminds/v4/controlled_terms/ethics_assessment.py
+++ openminds/v4/controlled_terms/ethics_assessment.py
@@ -177,3 +177,8 @@ EthicsAssessment.not_required = EthicsAssessment(
     description="An ethics assessment is 'not required' when no ethics approval was needed to conduct the study. This is typically true for all simulated and invertebrate data (except cephalopods).",
     name="not required",
 )
+EthicsAssessment.us_compliant = EthicsAssessment(
+    id="https://openminds.om-i.org/instances/ethicsAssessment/USCompliant",
+    definition="Data are ethically approved in compliance with the laws of the United States of America. No additional ethics assessment was made by the data sharing initiative.",
+    name="US compliant",
+)
