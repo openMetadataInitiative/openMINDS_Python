37c583a201a4afe0787af04e178be07d005d1a3f
openMINDS pipeline <openminds@ebrains.eu>
2026-05-04 14:55:28 +0000

    build triggered by workflow_dispatch

--- openminds/v3/sands/atlas/brain_atlas.py
+++ openminds/v3/sands/atlas/brain_atlas.py
@@ -5700,6 +5700,7 @@ BrainAtlas.whss_datlas = BrainAtlas(
             {
                 "@id": "https://openminds.ebrains.eu/instances/parcellationEntity/WHSSDatlas_cerebralCortexIncludingTheNeocortexAndTheHippocampus"
             },
+            {"@id": "https://openminds.ebrains.eu/instances/parcellationEntity/WHSSDatlas_cerebralCortex"},
             {"@id": "https://openminds.ebrains.eu/instances/parcellationEntity/WHSSDatlas_hippocampalRegion"},
             {"@id": "https://openminds.ebrains.eu/instances/parcellationEntity/WHSSDatlas_hippocampalFormation"},
             {
