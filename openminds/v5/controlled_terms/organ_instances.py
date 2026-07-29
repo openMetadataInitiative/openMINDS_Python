46fbc46e5ec9bc4f29a2b0625683e957cee1e6e6
openMINDS pipeline <openminds@ebrains.eu>
2026-07-29 12:11:08 +0000

    build triggered by workflow_dispatch

--- openminds/v5/controlled_terms/organ_instances.py
+++ openminds/v5/controlled_terms/organ_instances.py
@@ -8,8 +8,8 @@ from openminds.v5.controlled_terms.organ import Organ
 
 Organ.brain = Organ(
     id="https://openminds.om-i.org/instances/organ/brain",
-    definition="'Brain' is part of the central nervous system.",
-    description="The brain is the center of the nervous system in all vertebrate, and most invertebrate, animals. Some primitive animals such as jellyfish and starfish have a decentralized nervous system without a brain, while sponges lack any nervous system at all. In vertebrates, the brain is located in the head, protected by the skull and close to the primary sensory apparatus of vision, hearing, balance, taste, and smell[WP].",
+    definition="'Brain' is part of the central nervous system. [auto-generated from 'relationship' property of the [UBERON ontology term](http://purl.obolibrary.org/obo/UBERON_0000955)]",
+    description="The brain is the center of the nervous system in all vertebrate, and most invertebrate, animals. Some primitive animals such as jellyfish and starfish have a decentralized nervous system without a brain, while sponges lack any nervous system at all. In vertebrates, the brain is located in the head, protected by the skull and close to the primary sensory apparatus of vision, hearing, balance, taste, and smell. [definition of the [UBERON ontology term](http://purl.obolibrary.org/obo/UBERON_0000955)]",
     name="brain",
     other_ontology_identifiers=["http://uri.interlex.org/base/ilx_0101431"],
     preferred_cross_reference=IRI("https://knowledge-space.org/wiki/UBERON:0000955#brain-1"),
@@ -25,6 +25,33 @@ Organ.heart = Organ(
     preferred_cross_reference=IRI("https://knowledge-space.org/wiki/UBERON:0000948#heart"),
     preferred_ontology_identifier=IRI("http://purl.obolibrary.org/obo/UBERON_0000948"),
     synonyms=["chambered heart", "vertebrate heart"],
+)
+
+Organ.insect_adult_brain = Organ(
+    id="https://openminds.om-i.org/instances/organ/insectAdultBrain",
+    definition="Is a brain. Is part of the insect adult central nervous system. [auto-generated from properties of the [UBERON ontology term](http://purl.obolibrary.org/obo/UBERON_6003624) ('is_a' and 'relationship')]",
+    description="Brain of the adult. [definition of the [UBERON ontology term](http://purl.obolibrary.org/obo/UBERON_6003624)]",
+    name="insect adult brain",
+    preferred_cross_reference=IRI("https://knowledge-space.org/wiki/UBERON:6003624#adult-brain"),
+    preferred_ontology_identifier=IRI("http://purl.obolibrary.org/obo/UBERON_6003624"),
+)
+
+Organ.insect_embryonic_brain = Organ(
+    id="https://openminds.om-i.org/instances/organ/insectEmbryonicBrain",
+    definition="Is a brain. [auto-generated from 'is_a' property of the [UBERON ontology term](http://purl.obolibrary.org/obo/UBERON_6001060)]",
+    description="Brain of the embryo. [definition of the [UBERON ontology term](http://purl.obolibrary.org/obo/UBERON_6001060)]",
+    name="insect embryonic brain",
+    preferred_cross_reference=IRI("https://knowledge-space.org/wiki/UBERON:6001060#insect-embryonic-brain"),
+    preferred_ontology_identifier=IRI("http://purl.obolibrary.org/obo/UBERON_6001060"),
+)
+
+Organ.insect_embryonic_larval_brain = Organ(
+    id="https://openminds.om-i.org/instances/organ/insectEmbryonicLarvalBrain",
+    definition="Is a brain and ganglion of central nervous system. Is part of the insect embryonic/larval central nervous system. [auto-generated from properties of the [UBERON ontology term](http://purl.obolibrary.org/obo/UBERON_6001920) ('is_a' and 'relationship')]",
+    description="Brain of the embryo/larva. [definition of the [UBERON ontology term](http://purl.obolibrary.org/obo/UBERON_6001920)]",
+    name="insect embryonic/larval brain",
+    preferred_cross_reference=IRI("https://knowledge-space.org/wiki/UBERON:6001920#insect-embryonic-larval-brain"),
+    preferred_ontology_identifier=IRI("http://purl.obolibrary.org/obo/UBERON_6001920"),
 )
 
 Organ.liver = Organ(
@@ -46,6 +73,16 @@ Organ.muscle = Organ(
     preferred_ontology_identifier=IRI("http://purl.obolibrary.org/obo/UBERON_0001630"),
 )
 
+Organ.neuroendocrine_gland = Organ(
+    id="https://openminds.om-i.org/instances/organ/neuroendocrineGland",
+    definition="Is an anatomical entity. Is part of the nervous system. [auto-generated from properties of the [UBERON ontology term](http://purl.obolibrary.org/obo/UBERON_0010133) ('is_a' and 'relationship')]",
+    description="Any of the organized aggregations of cells that function as secretory or excretory organs and that release hormones in response to neural stimuli. [definition of the [UBERON ontology term](http://purl.obolibrary.org/obo/UBERON_0010133)]",
+    name="neuroendocrine gland",
+    preferred_cross_reference=IRI("https://knowledge-space.org/wiki/UBERON:0010133#neuroendocrine-gland"),
+    preferred_ontology_identifier=IRI("http://purl.obolibrary.org/obo/UBERON_0010133"),
+    synonyms=["neuroendocrine system gland"],
+)
+
 Organ.skin = Organ(
     id="https://openminds.om-i.org/instances/organ/skin",
     definition="'Skin' is the organ covering the body that consists of the dermis and epidermis.",
@@ -54,4 +91,13 @@ Organ.skin = Organ(
     preferred_cross_reference=IRI("https://knowledge-space.org/wiki/UBERON:0002097#skin-of-body"),
     preferred_ontology_identifier=IRI("http://purl.obolibrary.org/obo/UBERON_0002097"),
     synonyms=["entire skin", "skin organ"],
+)
+
+Organ.spinal_cord = Organ(
+    id="https://openminds.om-i.org/instances/organ/spinalCord",
+    definition="Is part of the central nervous system. [auto-generated from 'relationship' property of the [UBERON ontology term](http://purl.obolibrary.org/obo/UBERON_0002240)]",
+    description="Part of the central nervous system located in the vertebral canal continuous with and caudal to the brain; demarcated from brain by plane of foramen magnum. It is composed of an inner core of gray matter in which nerve cells predominate, and an outer layer of white matter in which myelinated nerve fibers predominate, and surrounds the central canal. (CUMBO) [definition of the [UBERON ontology term](http://purl.obolibrary.org/obo/UBERON_0002240)]",
+    name="spinal cord",
+    preferred_cross_reference=IRI("https://knowledge-space.org/wiki/UBERON:0002240#spinal-cord-1"),
+    preferred_ontology_identifier=IRI("http://purl.obolibrary.org/obo/UBERON_0002240"),
 )
