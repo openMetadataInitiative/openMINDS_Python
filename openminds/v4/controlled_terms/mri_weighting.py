5586664d9610750059a2b22c9ec0b20b5b837aff
openMINDS pipeline <openminds@ebrains.eu>
2026-08-19 12:43:07 +0000

    build triggered by workflow_dispatch

--- openminds/v4/controlled_terms/mri_weighting.py
+++ openminds/v4/controlled_terms/mri_weighting.py
@@ -102,3 +102,81 @@ class MRIWeighting(LinkedMetadata):
             preferred_ontology_identifier=preferred_ontology_identifier,
             synonyms=synonyms,
         )
+
+    @classmethod
+    def instances(cls):
+        return [value for value in cls.__dict__.values() if isinstance(value, cls)]
+
+    @classmethod
+    def by_name(
+        cls,
+        name: str,
+        match: str = "equals",
+        all: bool = False,
+        case_sensitive: bool = True,
+    ):
+        """
+        Search for instances in the openMINDS instance library based on their name.
+
+        This includes properties "name", "lookup_label", "family_name", "full_name", "short_name", "abbreviation", and "synonyms".
+
+        Note that not all metadata classes have a name.
+
+        Args:
+            name (str): a string to search for.
+            match (str, optional): either "equals" (exact match - default), "contains"
+                (the name-like property contains the given string), or "within"
+                (the given string contains the name-like property).
+            all (bool, optional): Whether to return all objects that match the name, or only the first. Defaults to False.
+            case_sensitive (bool, optional): Whether the search should be case-sensitive. Defaults to True.
+        """
+        namelike_properties = ("name", "lookup_label", "family_name", "full_name", "short_name", "abbreviation")
+        if cls._instance_lookup is None:
+            cls._instance_lookup = {}
+            for instance in cls.instances():
+                keys = []
+                for prop_name in namelike_properties:
+                    value = getattr(instance, prop_name, None)
+                    if value is not None:
+                        keys.append(value)
+                if hasattr(instance, "synonyms"):
+                    for synonym in instance.synonyms or []:
+                        keys.append(synonym)
+                for key in keys:
+                    if key in cls._instance_lookup:
+                        cls._instance_lookup[key].append(instance)
+                    else:
+                        cls._instance_lookup[key] = [instance]
+
+        def normalize(s):
+            return s if case_sensitive else s.casefold()
+
+        if match == "equals":
+            if case_sensitive:
+                matches = cls._instance_lookup.get(name, [])
+            else:
+                matches = []
+                for key, instances in cls._instance_lookup.items():
+                    if key.casefold() == name.casefold():
+                        matches.extend(instances)
+        elif match == "contains":
+            matches = []
+            for key, instances in cls._instance_lookup.items():
+                if normalize(name) in normalize(key):
+                    matches.extend(instances)
+        elif match == "within":
+            matches = []
+            for key, instances in cls._instance_lookup.items():
+                if normalize(key) in normalize(name):
+                    matches.extend(instances)
+        else:
+            raise ValueError("'match' must be either 'equals', 'contains', or 'within'")
+        if not matches:
+            return None
+        elif all:
+            return list(dict.fromkeys(matches))
+        else:
+            return matches[0]
+
+
+from . import mri_weighting_instances as _  # noqa: F401
